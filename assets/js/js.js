e479bfc440f4433c59edeba06ae4a358ec280c67
Kyle Franz <kyle@basisrobotics.tech>
2025-05-25 23:14:13 -0700

    python stuff

---
+++ assets/js/js.js
@@ -0,0 +1,70 @@
+$(document).ready(function() { Footnotes.setup(); });
+var Footnotes = { footnotetimeout: false, setup: function() { var footnotelinks = $("a[rel='footnote']")
+
+    footnotelinks.unbind('mouseover',Footnotes.footnoteover);
+    footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
+    
+    footnotelinks.bind('mouseover',Footnotes.footnoteover);
+    footnotelinks.bind('mouseout',Footnotes.footnoteoout);
+},
+footnoteover: function() {
+    clearTimeout(Footnotes.footnotetimeout);
+    $('#footnotediv').stop();
+    $('#footnotediv').remove();
+    
+    var id = $(this).attr('href').substr(1);
+    var position = $(this).offset();
+
+    var div = $(document.createElement('div'));
+    div.attr('id','footnotediv');
+    div.bind('mouseover',Footnotes.divover);
+    div.bind('mouseout',Footnotes.footnoteoout);
+
+    var el = document.getElementById(id);
+    div.html($(el).html());
+    div.find("a[rev='footnote']").remove();
+    
+    div.css({
+        position:'absolute',
+        width:'20em',
+        background:'#000000',
+        padding:'1em 1em 0em 1em',
+        border:'solid 1px',
+        'font-size':'90%',
+        'font-family': 'Helvetica, Sans-serif',
+        'line-height':1.4,
+        '-moz-border-radius':'.5em',
+        '-webkit-border-radius':'.5em',
+        'border-radius':'.5em',
+        transition: 'opacity 250ms ease-in, visibility 0ms ease-in 250ms;',
+        opacity:0.95
+    });
+    div.html($(el).html());
+    div.find("a[class='reversefootnote']").remove();
+    $(document.body).append(div);
+    
+
+    var left = position.left;
+    if(left + 420  > $(window).width() + $(window).scrollLeft())
+        left = $(window).width() - 420 + $(window).scrollLeft();
+    var top = position.top+20;
+    if(top + div.height() > $(window).height() + $(window).scrollTop())
+        top = position.top - div.height() - 15;
+    div.css({
+        left:left,
+        top:top
+    });
+},
+footnoteoout: function() {
+    Footnotes.footnotetimeout = setTimeout(function() {
+        $('#footnotediv').remove();
+    },100);
+},
+divover: function() {
+    clearTimeout(Footnotes.footnotetimeout);
+    $('#footnotediv').stop();
+    $('#footnotediv').css({
+            opacity: 0.95
+    });
+}
+}
